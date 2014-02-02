4d70c7dd7dcf78da1b12df0c7a64f9604d962e0c
tomByrer <tomByrer@gmail.com>
2014-02-01 23:00:29 -0700

    + remove-item-from-array, make array dense

--- fast-routines.js
+++ fast-routines.js
@@ -2,6 +2,7 @@
 
 
 // http://jsperf.com/treewalker-vs-dom-traversal/7
+// IE10 may be slower
 var walkWithRecursion = function (root, array) {
 	array.push(root);
 	var node = root.firstChild;
@@ -11,5 +12,17 @@ var walkWithRecursion = function (root, array) {
 	}
 }
 
+// http://jsperf.com/remove-item-from-array-splice-vs-lodash-without-working
+for (var i = 0, l = array.length; i < l; i++) {
+  if (array[i] === itemToRemove) {
+    array.splice(i, 1);
+    return;
+  }
+}
 
+// http://jsperf.com/sparse-to-dense array
+var i = 0;
+while (i !== array.length) {
+  i in array ? ++i : array.splice(i, 1);
+}
 ;
